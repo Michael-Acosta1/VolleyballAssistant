299eca13b09cf0e76f66b07cc1d5f1dbc081c6a1
Michael-Acosta1 <Micacost11@gmail.com>
2025-07-30 17:26:54 -0500

    update homepage with a picture in the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import React from "react";
+import images from "./assets/images";
+import { ImageBackground } from "react-native";
 const Header = ({ title }) => {
   return (
     <section>
-      <div className="headers">
+      <ImageBackground
+        source={images.myImage1}
+        style={{ width: "100%", height: 200 }}
+        resizeMode="cover"
+      >
         <div className="headerText">{title}</div>
-      </div>
+      </ImageBackground>
     </section>
   );
 };
---
+++ src/components/assets/images.js
@@ -0,0 +1,7 @@
+// images.js
+const images = {
+  myImage1: require("./vbNetPic.jpg"),
+  myImage2: require("./favicon.png"),
+};
+
+export default images;
